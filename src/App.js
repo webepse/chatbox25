e2bc4ac60ff6ffca7622a09706608393d895fdf1
Boderio <boderiotrigan@hotmail.com>
2024-12-11 11:53:04 +0100

    cours 5 - Transition CSS

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import Message from './components/Message';
 import { useParams } from 'react-router-dom';
 import database from './base'
 import { getDatabase, ref, set, remove, onValue } from 'firebase/database';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './animation.css'
 
 const App = () => {
   let {login} = useParams()
 
   const [pseudo, setPseudo] = useState(login)
   const [messages, setMessages] = useState({})
+  const messageRef = useRef()
+  // pour CSSTransition
+  const nodeRef = useRef()
 
   useEffect(()=>{
     console.log('test')
+    // const divMessage = document.querySelectorAll('.box')
+    // console.log(divMessage)
+    console.log(messageRef)
     // query
     const dbMessagesRef = ref(database,'messages')
     // écouter les event de changement des données (sur Firebase)
@@ -29,25 +37,39 @@ const App = () => {
   const addMessage = message => {
     const newMessages = {...messages}
     newMessages[`message-${Date.now()}`] = message
+    Object.keys(newMessages).slice(0,-10).forEach(key=>{
+      newMessages[key] = null
+    })
     // setMessages(newMessages)
     set(ref(database,'/'),{
       messages: newMessages
     })
   }
 
+  const isUser = paramPseudo => paramPseudo === pseudo
+
   const myMessages = Object.keys(messages).map(key => (
-    <Message 
+    <CSSTransition
       key={key}
-      pseudo={messages[key].pseudo}
-      message={messages[key].message}
-    />
+      timeout={200}
+      classNames={'fade'}
+      nodeRef={nodeRef}
+    >
+      <Message 
+        pseudo={messages[key].pseudo}
+        message={messages[key].message}
+        isUser={isUser}
+      />
+    </CSSTransition>
   ))
 
   return ( 
     <div className="box">
       <div>
-        <div className="messages">
-          {myMessages}
+        <div className="messages" ref={messageRef}>
+          <TransitionGroup className="message">
+            {myMessages}
+          </TransitionGroup>
         </div>
       </div>
       <Formulaire 
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,18 @@
-const Message = ({pseudo,message}) => {
-    return ( 
-        <p className="user-message">
-            {message}
-        </p>
-     );
+const Message = ({pseudo,message,isUser}) => {
+    if(isUser(pseudo))
+    {
+        return ( 
+            <p className="user-message">
+                {message}
+            </p>
+         );
+    }else{
+        return ( 
+            <p className="not-user-message">
+                <strong>{pseudo}:</strong> {message}
+            </p>
+         );
+    }
 }
  
 export default Message;
