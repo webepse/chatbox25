ec387a2dbed38a25089d172e6b8fc394855b2691
Boderio <boderiotrigan@hotmail.com>
2024-12-11 10:59:08 +0100

    cours 5 - connexion à Firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css'
 import Formulaire from './components/Formulaire';
 import Message from './components/Message';
 import { useParams } from 'react-router-dom';
+import database from './base'
+import { getDatabase, ref, set, remove, onValue } from 'firebase/database';
 
 const App = () => {
   let {login} = useParams()
@@ -10,30 +12,29 @@ const App = () => {
   const [pseudo, setPseudo] = useState(login)
   const [messages, setMessages] = useState({})
 
+  useEffect(()=>{
+    console.log('test')
+    // query
+    const dbMessagesRef = ref(database,'messages')
+    // écouter les event de changement des données (sur Firebase)
+    onValue(dbMessagesRef, (snapshot) => {
+      const data = snapshot.val()
+      if(data)
+      {
+        setMessages(data)
+      }
+    })
+  },[])
+
   const addMessage = message => {
     const newMessages = {...messages}
     newMessages[`message-${Date.now()}`] = message
-    setMessages(newMessages)
+    // setMessages(newMessages)
+    set(ref(database,'/'),{
+      messages: newMessages
+    })
   }
 
-  /*
-    messages = [
-      'message-123456489' => {
-          pseudo: "Jordan",
-          message: 'test'
-        },
-      'message-1561956156' => {
-          pseudo: "Carole",
-          message: "Hello"  
-      },
-      'message-15915991' => {
-          pseudo: "Nimai",
-          message: "hello"
-        }
-    ]
-  */
-  /* crée un tableau avec les clé d'un autre tableau ['message-123456489','message-1561956156','message-15915991'] */
-  // map recrée un tableau sur base d'un tableau qui bouclé
   const myMessages = Object.keys(messages).map(key => (
     <Message 
       key={key}
